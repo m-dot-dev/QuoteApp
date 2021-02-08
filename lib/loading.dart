89c516dc563cb2b7ee8bed0fefcccba7bbd9297a
Muhammad Talha <muhammadtalha517@gmail.com>
2021-02-08 17:16:47 +0500

    Updated Project

--- lib/loading.dart
+++ lib/loading.dart
@@ -12,15 +12,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Future<void> getData() async{
       Response response;
       Map data;
+      setState(() {
+        display = 'Loading Quote Data';
+      });
       response = await get('https://api.quotable.io/random');
       data = jsonDecode(response.body);
+      setState(() {
+        display = 'Quote Data Loaded';
+      });
+      setState(() {
+        display = 'Loading Background Image';
+      });
       response = await get('https://api.unsplash.com/photos/random/?collections=151521?&client_id=Ul-qaLX1UyFLlgtquzfaKkrtrigQnxC7_lyizWj7SBE');
       try {
         data['image'] =jsonDecode(response.body)['urls']['regular'];
       }catch(e){
         data['image'] = 'https://th.bing.com/th/id/OIP.glyJhf2YbvvUG5nzUT4QWwDOEs?pid=Api&rs=1';
       }
-
+      setState(() {
+        display = 'Content Load Complete';
+      });
       if(data['content']==null ){
         data['content'] = 'SORRY, COULD NOT FETCH DATA';
         data['author'] = '';
@@ -38,7 +49,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     super.initState();
     getData();
   }
-
+  String display = "Loading A Random Quote";
   @override
   Widget build(BuildContext context) {
 
@@ -47,17 +58,60 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20.0),
-          child: Center(
-            child: SpinKitCircle(
-                color: Colors.white,
-                size: 100.0,
-            )
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                display,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              Center(
+                child: SpinKitCircle(
+                    color: Colors.white,
+                    size: 100.0,
+                )
+              ),
+            ],
           ),
         ),
       )
     );
   }
 }
+
+class LoadingBlank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blueAccent[700],
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Loading Quote",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                Center(
+                    child: SpinKitCircle(
+                      color: Colors.white,
+                      size: 100.0,
+                    )
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
 
 class LoadingCategories extends StatefulWidget {
   @override
@@ -94,12 +148,23 @@ class _LoadingCategoriesState extends State<LoadingCategories> {
         body: SafeArea(
         child: Padding(
         padding: EdgeInsets.all(20.0),
-          child: Center(
-          child: SpinKitCircle(
-          color: Colors.white,
-          size: 100.0,
-          )
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                  "Loading Quote Categories",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+              ),
+              Center(
+              child: SpinKitCircle(
+                color: Colors.white,
+                size: 100.0,
+              )
         ),
+            ],
+          ),
       ),
     )
     );
